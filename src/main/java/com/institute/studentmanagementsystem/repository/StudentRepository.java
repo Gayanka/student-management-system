1b174c505ab2ef5735965890b6b57d1fa4f43cf3
Gayanka <gayanka1990@gmail.com>
2023-03-31 22:59:13 +1300

    DB Docker file added

--- src/main/java/com/institute/studentmanagementsystem/repository/StudentRepository.java
+++ src/main/java/com/institute/studentmanagementsystem/repository/StudentRepository.java
@@ -1,4 +1,10 @@
 package com.institute.studentmanagementsystem.repository;
 
-public interface StudentRepository {
+import com.institute.studentmanagementsystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
 }
