e633bd976e0c30f31c613d15737722b5763be697
Gayanka <gayanka1990@gmail.com>
2023-04-01 22:40:19 +1300

    get All students api added

--- src/main/java/com/institute/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/institute/studentmanagementsystem/controller/StudentController.java
@@ -2,7 +2,6 @@ package com.institute.studentmanagementsystem.controller;
 
 import com.institute.studentmanagementsystem.model.Student;
 import com.institute.studentmanagementsystem.service.StudentService;
-import org.apache.catalina.LifecycleState;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/student")
+@CrossOrigin
 public class StudentController {
 
     @Autowired
